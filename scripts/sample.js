e62ed6343833e665b9c09b2105e6fca4e2efb013
Coa <meecorcll@gmail.com>
2022-08-27 19:24:32 +0800

    dasd

--- scripts/sample.js
+++ scripts/sample.js
@@ -7,3 +7,33 @@ button.addEventListener("click", (e) => {
 
     counter.innerText = ++currentCounter; 
 })
+
+
+
+//factory function. 
+const user = (name) => {
+    // const sayName = ()=> {
+    //     console.log(name);
+    // }
+    return {name}
+} 
+
+
+//module pattern.
+const create = (()=>{
+    let users = []
+    const userName  = document.querySelector("#name"); 
+    // let lastName = document.querySelector("#lastName");   
+    const meeco = user(userName); 
+    const qrcode = new QRCode(document.querySelector("#qrcode"));
+    const render = () => {
+        generateQr();       
+    function generateQr () {
+        console.log(meeco.userName)
+        qrcode.makeCode(userName.value)
+    }
+    }
+    return {render}
+})();
+
+// create.render()
