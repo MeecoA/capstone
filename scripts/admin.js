f16aa1c1b8060dcb7dd2b050faee249a1fee179c
Coa <meecorcll@gmail.com>
2022-08-31 14:05:21 +0800

    initia commit

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,3 +1,15 @@
+//for the data table 
+
+$(document).ready( function () {
+  $('table.display').DataTable();} );
+
+
+  $(document).ready( function () {
+    $('#secTable').DataTable();} );
+
+
+//for dropdown content
+
 let dropDown = document.querySelectorAll(".menu-btn"); 
 
 dropDown.forEach(function(item) {
@@ -11,3 +23,53 @@ dropDown.forEach(function(item) {
         }
     });
 })
+let headerTitle = document.querySelector("#headerTitle");
+//ajax - users side bar
+function loadSec() {
+  headerTitle.textContent = "Security"
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
+    }
+  }; 
+  xhttp.open("GET", "/sidebar/user-security.html",true);
+  xhttp.send();
+}
+function loadPersonnel() {
+  headerTitle.textContent = "Personnel"
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
+    }
+  }; 
+  xhttp.open("GET", "/sidebar/user-personnel.html",true);
+  xhttp.send();
+}
+function loadResidents() {
+  headerTitle.textContent = "Residents"
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
+    }
+  }; 
+  xhttp.open("GET", "/sidebar/user-resident.html",true);
+  xhttp.send();
+}
+
+function loadVisitors() {
+  headerTitle.textContent = "Visitors"
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML = this.responseText;
+    }
+  }; 
+  xhttp.open("GET", "/sidebar/user-visitor.html",true);
+  xhttp.send();
+}
+
+//changing header when sidebar link is clicked
+
