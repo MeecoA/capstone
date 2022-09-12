858be50ee173c06275efde0ee8c025e71da85ad2
Coa <meecorcll@gmail.com>
2022-09-12 13:37:59 +0800

    hello

--- scripts/user.js
+++ scripts/user.js
@@ -4,13 +4,10 @@ function loadAcc() {
     xhttp.onreadystatechange = function() {
       if(this.readyState == 4 && this.status == 200) {
         document.getElementById('content').innerHTML = this.responseText;
-        let buttonS = document.querySelector('.sample');
-        buttonS.addEventListener('click', () => {
-          console.log("hi");
-        })
+
       }
     };   
-  
+
     xhttp.open("GET", "account.html",true);
     xhttp.send();
 
