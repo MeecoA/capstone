05017975ea09be30a720d857f624880ae3c35066
Coa <meecorcll@gmail.com>
2022-10-06 08:50:11 +0800

    initital commit

--- src/index.js
+++ src/index.js
@@ -12,19 +12,28 @@ import {
   orderBy,
   serverTimestamp,
   getDoc,
+  updateDoc,
 } from "firebase/firestore";
 
 //this config connects the backend and frontend
 //after this, intall firebase in node.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBY3hnXsQuXX_RIJ0VZSWbIYFmOxYe94SQ",
-  authDomain: "sample-capstone-project-ba941.firebaseapp.com",
-  projectId: "sample-capstone-project-ba941",
-  storageBucket: "sample-capstone-project-ba941.appspot.com",
-  messagingSenderId: "619482030443",
-  appId: "1:619482030443:web:f0d4435fed137c16902c2e",
-  measurementId: "G-VNRXHHQSRB",
+  apiKey: "AIzaSyBFzmDkFR_ZIi5aSc1ATfXykOcowRTx8oA",
+  authDomain: "bulsu---pms.firebaseapp.com",
+  projectId: "bulsu---pms",
+  storageBucket: "bulsu---pms.appspot.com",
+  messagingSenderId: "36091561292",
+  appId: "1:36091561292:web:85d41dea4e7c7b80f8fbe9",
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBY3hnXsQuXX_RIJ0VZSWbIYFmOxYe94SQ",
+//   authDomain: "sample-capstone-project-ba941.firebaseapp.com",
+//   projectId: "sample-capstone-project-ba941",
+//   storageBucket: "sample-capstone-project-ba941.appspot.com",
+//   messagingSenderId: "619482030443",
+//   appId: "1:619482030443:web:f0d4435fed137c16902c2e",
+//   measurementId: "G-VNRXHHQSRB",
+// };
 
 //for initializing app
 initializeApp(firebaseConfig);
@@ -39,7 +48,7 @@ const colRef = collection(db, "security");
 const q = query(colRef, orderBy("createdAt"));
 
 const loadSec = document.querySelector("#secLink");
-
+let id;
 //AJAX
 loadSec.addEventListener("click", () => {
   headerTitle.textContent = "Users";
@@ -75,30 +84,84 @@ loadSec.addEventListener("click", () => {
       }); //end adding data
 
       //creating the table data
-
+      let id;
       const sectable = document.querySelector(".eminem");
       const renderSecurity = (docu) => {
         console.log(docu.id);
 
         const tr = `<tr data-id='${docu.id}'>
+          <td>${docu.id}</td>
           <td>${docu.data().firstname} ${docu.data().lastname}</td>
           <td>${docu.data().position}</td>
           <td>${docu.data().barangay}, ${docu.data().street}, ${docu.data().municipality}, ${docu.data().province}</td>
           <td>${docu.data().email}</td>
           <td>${docu.data().phone}</td>
           <td>
-            <a href="#editmodal" rel="modal:open"><button>Edit</button></a>
-            <button class="secDelete">Delete</button>
+            <div class = "actions-button">
+            <div class="view-button td-btn">
+            <iconify-icon
+            class="view-icon"
+            icon="ci:edit" style="color: black;" width="16" height="16"></iconify-icon>
+            <a href="#editmodal" rel="modal:open">Edit</a></div>
+            <div class="delete-button">
+            <iconify-icon
+              class="view-icon"
+              icon="ep:delete-filled"
+              style="color: white"
+              width="16"
+              height="16"
+            ></iconify-icon>
+            <div>Delete</div>
+          </div>
+            </div>
           </td>
         </tr>`;
         sectable.insertAdjacentHTML("beforeend", tr);
         //deleting data
-        const secDelete = document.querySelector(`[data-id='${docu.id}'] .secDelete`);
+        const secDelete = document.querySelector(`[data-id='${docu.id}'] .delete-button`);
         secDelete.addEventListener("click", () => {
           const docRef = doc(db, "security", docu.id);
           deleteDoc(docRef).then(() => {
             console.log("deleted successfully");
           });
+        }); //end of deleting data
+
+        //editing data
+        const editSecForm = document.querySelector("#editSecForm");
+        const editSecBtn = document.querySelector(`[data-id='${docu.id}'] .view-button`);
+
+        editSecBtn.addEventListener("click", () => {
+          id = docu.id;
+          editSecForm.secBrgy.value = docu.data().barangay;
+          editSecForm.position.value = docu.data().position;
+          editSecForm.secEmail.value = docu.data().email;
+          editSecForm.secFname.value = docu.data().firstname;
+          editSecForm.secLname.value = docu.data().lastname;
+          editSecForm.secMname.value = docu.data().middlename;
+          editSecForm.secMunicip.value = docu.data().municipality;
+          editSecForm.secPassword.value = docu.data().password;
+          editSecForm.secPhone.value = docu.data().phone;
+          editSecForm.secProvince.value = docu.data().province;
+          editSecForm.secStreet.value = docu.data().street;
+        });
+
+        //for edit submit
+        editSecForm.addEventListener("submit", (e) => {
+          e.preventDefault();
+          const docRef = doc(db, "security", id);
+
+          updateDoc(docRef, {
+            firstname: editSecForm.secFname.value,
+            middlename: editSecForm.secMname.value,
+            lastname: editSecForm.secLname.value,
+            position: editSecForm.position.value,
+            email: editSecForm.secEmail.value,
+            phone: editSecForm.secPhone.value,
+            province: editSecForm.secProvince.value,
+            street: editSecForm.secStreet.value,
+            municipality: editSecForm.secMunicip.value,
+            barangay: editSecForm.secBrgy.value,
+          }).then(() => {});
         });
       }; //end of render sec
 
@@ -114,6 +177,11 @@ loadSec.addEventListener("click", () => {
             let row = document.querySelector(`[data-id="${change.doc.id}"]`);
             // let tbody = row.parentElement;
             sectable.removeChild(row);
+          }
+          if (change.type === "modified") {
+            let row = document.querySelector(`[data-id="${change.doc.id}"]`);
+            sectable.removeChild(row);
+            renderSecurity(change.doc);
           }
           security.push({ ...change.doc.data(), id: change.doc.id });
         });
